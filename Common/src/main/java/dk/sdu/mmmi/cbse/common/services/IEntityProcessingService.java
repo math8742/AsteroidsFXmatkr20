579b0ed80571c03041f62046879014c74b42a6fe
matae <mataeosp@gmail.com>
2025-05-22 09:40:44 +0200

    javadoc documentation for services with pre and post conditions

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
@@ -4,14 +4,23 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 public interface IEntityProcessingService {
-
     /**
-     *
-     *
+     * Processes entities according to game rules in current frame
      *
      * @param gameData
      * @param world
-     * @throws
+     *
+     * Preconditions:
+     * gameData is not null
+     * world is not null
+     * world.getEntities returns not null collection of game entities
+     * Entities in world.getEntities are in a valid state to pe processed
+     *
+     * Postconditions:
+     * Entities are modified based on game rules specified via IEntityProcessingService
+     * Entities remain in valid state to be processed
+     *
+     * @throws NullPointerException if gameData, world or world.getEntities is null or returns null
      */
     void process(GameData gameData, World world);
 }
