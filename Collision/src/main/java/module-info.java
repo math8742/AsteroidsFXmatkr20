3fc0273aa93e1dd32bf44722e9103787ccc0a6a2
matae <mataeosp@gmail.com>
2025-05-29 14:24:54 +0200

    module info fix

--- Collision/src/main/java/module-info.java
+++ Collision/src/main/java/module-info.java
@@ -3,7 +3,6 @@ import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
 module Collision {
     requires Common;
-    requires CommonBullet;
     uses dk.sdu.mmmi.cbse.common.components.Health;
     provides IPostEntityProcessingService with dk.sdu.mmmi.cbse.collision.Collision;
 }
